224b4d7d6b7e41a8b0c763e8e99854e011e9d322
Tusto <tusto97@gmail.com>
2023-07-16 15:58:49 +0100

    Anotacoes de validacao do ProductDTO

--- src/main/java/ricardo/projetos/product/ms/dto/ProductDTO.java
+++ src/main/java/ricardo/projetos/product/ms/dto/ProductDTO.java
@@ -1,13 +1,22 @@
 package ricardo.projetos.product.ms.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ProductDTO {
 
+    @NotBlank
     private String name;
+
+    @Size(min = 50)
     private String description;
+
+    @Positive
     private double price;
+
     private boolean isAvailable;
 }
