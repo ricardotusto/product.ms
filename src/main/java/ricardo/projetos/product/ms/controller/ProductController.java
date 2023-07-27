33191971102c2f9a6f71c579106e6a2892236719
Tusto <tusto97@gmail.com>
2023-07-27 12:34:19 +0100

    Product endpoint getAll()

--- src/main/java/ricardo/projetos/product/ms/controller/ProductController.java
+++ src/main/java/ricardo/projetos/product/ms/controller/ProductController.java
@@ -4,13 +4,11 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ricardo.projetos.product.ms.dto.ProductDTO;
 import ricardo.projetos.product.ms.service.ProductService;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -28,5 +26,10 @@ public class ProductController {
         }
 
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAll(){
+        return ResponseEntity.ok(service.getAll());
     }
 }
